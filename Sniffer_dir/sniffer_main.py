ff88a2f68acf7a50dcc503bd50af23d287254f67
kirill050 <kirilltikhonov050@gmail.com>
2024-01-07 17:25:45 +0300

    v1.2.1 (fixed short time analyzing)

--- Sniffer_dir/sniffer_main.py
+++ Sniffer_dir/sniffer_main.py
@@ -18,7 +18,7 @@ def ScanNetworks(sniffer: Sniffer, channels: list[int]):
 	wifiscaner = WIFIScanner(sniffer)
 	for i in channels:
 		wifiscaner.sniffer.SetChannel(i)
-		wifiscaner.scan(timeout=10)
+		wifiscaner.scan(timeout=7)
 
 	networks = wifiscaner.GetNetworksInfo() 
 	# пример итерации по информации о сетях
@@ -51,7 +51,7 @@ def sniffer_start_RTS_analyzer(interface, attacking_addr, target_addr, channel: 
 	a = RTS_Analyzer(1)
 	collector_thread = threading.Thread(target=a.Analyzer, args=(packets_q, attacking_addr, target_addr), daemon=True).start()
 	printer_thread = threading.Thread(target=a.Printer, args=(target_addr,), daemon=True).start()
-	sniffer.exec(prn=MonitorCts, timeout=100)
+	sniffer.exec(prn=MonitorCts)#, timeout=100)
 	print("Started network scanning")
 
 
@@ -68,7 +68,7 @@ def sniffer_start_AP_analyzer(interface, bssid_real, bssid_fake, channel: int = 
 	a = AP_Analyzer(bssid_real=bssid_real, bssid_fake=bssid_fake)
 	collector_thread = threading.Thread(target=a.Analyzer, args=(packets_q, ), daemon=True).start()
 	printer_thread = threading.Thread(target=a.Printer, args=(bssid_real,), daemon=True).start()
-	sniffer.exec(prn=MonitorCts, timeout=100)
+	sniffer.exec(prn=MonitorCts)#, timeout=100)
 	print("Started network scanning")
 
 def changing_channels(sniffer: Sniffer, channels: list[int]):
@@ -90,7 +90,7 @@ def sniffer_start_NPR_Analyzer(interface, targets, channels: list[int]):
 	a = NPR_Analyzer(targets=targets)
 	collector_thread = threading.Thread(target=a.Analyzer, args=(packets_q, ), daemon=True).start()
 	printer_thread = threading.Thread(target=a.Printer, args=(targets,), daemon=True).start()
-	sniffer.exec(prn=MonitorCts, timeout=100)
+	sniffer.exec(prn=MonitorCts)#, timeout=100)
 	print("Started network scanning")
 
 def sniffer_start_Deauth_Analyzer(interface, targets, channels: list[int]):
@@ -106,5 +106,5 @@ def sniffer_start_Deauth_Analyzer(interface, targets, channels: list[int]):
 	a = Deauth_Analyzer(targets=targets)
 	collector_thread = threading.Thread(target=a.Analyzer, args=(packets_q, ), daemon=True).start()
 	printer_thread = threading.Thread(target=a.Printer, args=(targets,), daemon=True).start()
-	sniffer.exec(prn=MonitorCts, timeout=100)
+	sniffer.exec(prn=MonitorCts)#, timeout=100)
 	print("Started network scanning")
